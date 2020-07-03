499fa4162b5fbbc1925b79a6a6bbd09f10b26916
mhart <mhartj@protonmail.com>
2020-07-04 01:57:32 +0200

    Added the encryption part

--- prjrem.py
+++ prjrem.py
@@ -8,12 +8,13 @@ class PrjRem:
     CHAR_SET_LENGTH = len(CHAR_SET) - 1
     PATH_HOME = Path.home().as_posix() + '/.prjrem'
     PATH_CONF = PATH_HOME + '/config'
+    PSW = b'1234123412341234'
 
     def __init__(self):
-        self.config = dict()
-        self.passwords = dict()
         self.rng = secrets.SystemRandom()
-        self.configDefaults = {'location': self.PATH_HOME + '/.prjremDat'}
+        self.configDefaults = {'location': self.PATH_HOME + '/prjremDat'}
+        self.config = self.configDefaults
+        self.passwords = dict()
 
 
     def getSortedKeys(self):
@@ -33,7 +34,8 @@ class PrjRem:
     def readConf(self):
         if Path(self.PATH_CONF).exists():
             with Path(self.PATH_CONF).open() as f:
-                self.config = json.load(f)
+                for k,v in json.load(f).items():
+                    self.config[k] = v
         else:
             Path(self.PATH_HOME).mkdir(exist_ok=True)
 
@@ -42,18 +44,32 @@ class PrjRem:
             f.write(json.dumps(self.config))
 
     def readPass(self):
-        s = ''
-        with Path(self.config['location']).open() as f:
-            s = f.read()
-        
-        # TODO aes
-        self.passwords = json.loads(s)
+        '''Open password file and attempt decryption'''
+        if not Path(self.config['location']).exists():
+            return 1
+
+        with Path(self.config['location']).open(mode='rb') as f:
+            bStream = f.read()
+
+        try:
+            cipher = AES.new(self.PSW, AES.MODE_CBC, bStream[:16])
+            self.passwords = json.loads(cipher.decrypt(bStream[16:]))
+            return 0
+        except Exception as e:
+            self.error = e
+
+        return 1
 
     def savePass(self):
-        s = json.dumps(self.passwords)
-        with Path(self.config['location']).open(mode='w') as f:
-            f.write(s)
-
+        '''Convert passwords to JSON and encrypt with CBC\n
+        Use a random IV, prepend it and use trailing 0's as padding'''
+        cipher = AES.new(self.PSW, AES.MODE_CBC, bytearray(self.sequence(16), 'utf_8'))
+        stream = json.dumps(self.passwords)
+        stream.join('0' * (32 - (len(stream) % 16)))
+        bStream = cipher.encrypt(bytearray(stream, 'utf_8'))
+        with Path(self.config['location']).open(mode='wb') as f:
+            f.write(cipher.iv)
+            f.write(bStream)
 
     # Commands
     def make(self, usr, psw=None):
@@ -93,6 +109,16 @@ class PrjRemCMD(cmd.Cmd):
 
     def do_info(self, arg):
         print(self.program.config)
+
+    def do_save(self, arg):
+        for x in range(0,4):
+            self.program.passwords['ent%s' % x] = self.program.sequence(16)
+
+        self.program.savePass()
+
+    def do_read(self, arg):
+        self.program.readPass()
+        print(self.program.passwords)
 
 if __name__ == '__main__':
     PrjRemCMD().cmdloop()
