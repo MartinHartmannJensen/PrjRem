38dfda0ed1a9c8dc35f418aeb81ecaaf044d5b6a
mhart <mhartj@protonmail.com>
2020-07-13 20:33:52 +0200

    Password prompt
    Docstrings
    Naming conventions
    and CLI command changes

--- prjrem.py
+++ prjrem.py
@@ -17,7 +17,7 @@ class PrjRem:
         self.configDefaults = {'location': self.PATH_HOME + '/prjremDat'}
         self.config = self.configDefaults
         self.passwords = dict()
-        self.psw = b'1234123412341234'
+        self.psw = '0000000000000000'
         self.error = 'Unknown error'
 
     def getSortedKeys(self):
@@ -26,6 +26,15 @@ class PrjRem:
 
     def setPassLocation(self, path):
         self.config['location'] = path
+
+    def setPsw(self, psw):
+        '''Set a password for the context. Also pad it'''
+        if psw == '':
+            self.error = 'Abort!'
+            return 1
+
+        self.psw = ''.join([psw, '0' * (32 - (len(psw) % 16))])
+        return 0
 
     def sequence(self, length):
         '''Generate sequence of randomized characters'''
@@ -57,11 +66,12 @@ class PrjRem:
             return 1
 
         with Path(self.config['location']).open(mode='rb') as f:
-            bStream = f.read()
+            es = f.read()
 
         try:
-            cipher = AES.new(bytearray(self.psw, self.ENC), AES.MODE_CBC, bStream[:16])
-            self.passwords = json.loads(cipher.decrypt(bStream[16:]))
+            cipher = AES.new(bytearray(self.psw, self.ENC), AES.MODE_CBC, es[:16])
+            bs = cipher.decrypt(es[16:])
+            self.passwords = json.loads(bs.rstrip(b'0'))
             return 0
         except Exception as e:
             self.error = e
@@ -71,16 +81,23 @@ class PrjRem:
     def savePass(self):
         '''Convert passwords to JSON and encrypt with CBC\n
         Use a random IV, prepend it and use trailing 0's as padding'''
-        cipher = AES.new(bytearray(self.psw, self.ENC), AES.MODE_CBC, bytearray(self.sequence(16), self.ENC))
+        iv = bytearray(self.sequence(16), self.ENC)
+        cipher = AES.new(bytearray(self.psw, self.ENC), AES.MODE_CBC, iv)
         stream = json.dumps(self.passwords)
-        stream.join('0' * (32 - (len(stream) % 16)))
-        bStream = cipher.encrypt(bytearray(stream, self.ENC))
-        with Path(self.config['location']).open(mode='wb') as f:
-            f.write(cipher.iv)
-            f.write(bStream)
+        stream = ''.join([stream, '0' * (32 - (len(stream) % 16))])
+        es = cipher.encrypt(bytearray(stream, self.ENC))
+        try:
+            with Path(self.config['location']).open(mode='wb') as f:
+                f.write(iv)
+                f.write(es)
+        except Exception as e:
+            self.error = e
+            return 1
+
+        return 0
 
     # Commands
-    def make(self, usr, psw=None):
+    def cmd_make(self, usr, psw=None):
         '''Creates a new password\n
         Returns 0 on success'''
         self.error = 'Arguments may only contain numbers, letters and the special characters: %s' % self.SYMBOLS
@@ -94,7 +111,7 @@ class PrjRem:
         self.passwords[usr] = psw
         return 0
 
-    def retrieve(self, identifier):
+    def cmd_retrieve(self, identifier):
         '''Looks up a password by key or number\n
         Returns Tuple with Key and Password or None'''
         if identifier in self.passwords:
@@ -109,12 +126,15 @@ class PrjRem:
 
         return None
 
+    def cmd_listToPrint(self):
+        '''Formatted string with newlines for each sorted key'''
+        return ''.join(['%s) %s\n' % (idx, key) for idx, key in enumerate(self.getSortedKeys())]) 
 
 
 # UI
 class PrjRemCMD(cmd.Cmd):
-    intro = 'Project Remembrance\'s Commandline Interface'
-    prompt = '> '
+    intro = '====\nProject Remembrance\'s Commandline Interface\n===='
+    prompt = 'PrjRem no-file> '
     file = None
 
     def emptyline(self):
@@ -123,33 +143,31 @@ class PrjRemCMD(cmd.Cmd):
     def can_exit(self):
         return True
 
-    def do_exit(self, e):
-        '''Write passwords to set location and exit the program'''
-        print('Writing to file. Please wait.')
-        self.program.savePass()
-        return True
-    
-    do_EOF = do_exit
-    do_q = do_exit
-    do_quit = do_exit
-
     def preloop(self):
         '''Read config, prompt for encryption key and decrypt password file'''
         self.program = PrjRem()
         self.program.readConf()
-        # self.program.psw = getpass.getpass('Enter password: ')
+        self.program.setPsw(getpass.getpass('Enter password: '))
+        print('Attempting to read:\n    %s' % self.program.config['location'])
         if 0 == self.program.readPass():
+            print('Success, listing passwords\n')
             self.do_list('')
-            print('Loaded %s' % self.program.config['location'])
+            self.prompt = 'PrjRem %s> ' % self.program.config['location']
+        else:
+            print('Something went wrong:\n    %s' % self.program.error)
 
     def default(self, line):
         '''Try and interpret line as a key to a stored password'''
         self.do_retrieve(line)
 
     def do_retrieve(self, line):
-        '''Try and interpret line as a 'usr' key to retrieve a password 
-        \nRetrieve is the default line command.'''
-        psw = self.program.retrieve(line)
+        '''
+        > retrieve identifier
+        Get stored password. "Identifier" can either be a "usr" key or a number
+        from the "list" command.
+        This is the default line command. "Retrieve" can be excluded.
+        '''
+        psw = self.program.cmd_retrieve(line)
         if psw is None:
             print(self.program.error)
         else:
@@ -157,43 +175,63 @@ class PrjRemCMD(cmd.Cmd):
             pyperclip.copy(psw[1])
 
     def do_make(self, arg, psw = None):
-        '''make usr [psw]\n
-        Creates a new password stored under the "usr" key.\n
-        "psw" is optional. If left out a random password is generated.'''
+        '''
+        > make usr [psw]
+        Create a new password which is stored under the "usr" key.
+        "psw" is optional. If left out a random password is generated.
+        '''
         args = arg.split()
         if len(args) > 0:
             if len(args) > 1:
                 psw = args[1]
-            if 0 == self.program.make(args[0], psw):
+            if 0 == self.program.cmd_make(args[0], psw):
                 print('%s made!' % args[0])
                 pyperclip.copy(self.program.passwords[args[0]])
             else:
                 print(self.program.error)
 
-    def do_gen(self, arg):
-        print(self.program.sequence(16))
-
-    def do_info(self, arg):
-        print(self.program.config)
-
-    def do_save(self, arg):
-        for x in range(0,4):
-            self.program.passwords['ent%s' % x] = self.program.sequence(16)
-
-        self.program.savePass()
-
-    def do_read(self, arg):
-        self.program.readPass()
-        print(self.program.passwords)
-    
     def do_list(self, arg):
-        ident = 0
-        for x in self.program.getSortedKeys():
-            print('%s) %s' % (ident, x))
-            ident += 1
+        '''
+        > list
+        List all "usr" keys in a sorted print.
+        '''
+        print(self.program.cmd_listToPrint())
 
-    def do_check(self, s):
-        print(self.program.isLegit(s))
+    def do_psw(self, arg):
+        '''
+        > psw
+        Enter a new password. This is the encryption key and is not stored anywhere.
+        It is used for the current context only and applies on the next write.
+        Remember well.
+        '''
+        if 0 < self.program.setPsw(getpass.getpass('Enter password: ')):
+            print(self.program.error)
+
+    def do_loc(self, arg):
+        '''
+        > loc path
+        Changes password file location.
+        '''
+        args = arg.split()
+        if len(args) > 0:
+            self.program.setPassLocation(args[0])
+            self.prompt = 'PrjRem %s> ' % self.program.config['location']
+
+    def do_exit(self, e):
+        '''
+        Write passwords to current location and exit the program.
+        '''
+        if len(self.program.passwords) > 0:
+            print('Writing to file. Please wait.')
+            if 0 < self.program.savePass():
+                print(self.program.error)
+            else:
+                return True
+
+    do_EOF = do_exit
+    do_q = do_exit
+    do_quit = do_exit
+
 
 if __name__ == '__main__':
     PrjRemCMD().cmdloop()
